f44fdca7439885cef7a683c96862d401ab27ab37
Ahmed Shaban Helwa <ahmedshabaan@outlook.com>
2022-06-17 18:48:05 +0200

    Feature 2: Cycle 3: Refactor: Switch-code smell: 2. Decouple the methods introduced from the MarsRover class by returning new instances of Position.

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,7 +1,7 @@
 class MarsRover {
 
     private final String initialState;
-    private final Position position;
+    private Position position;
 
     public MarsRover(String initialState) {
         this.initialState = initialState;
@@ -17,29 +17,33 @@ class MarsRover {
 
         if (commands.equals("M")) {
             switch (direction) {
-                case "N" -> moveNorth();
-                case "E" -> moveEast();
-                case "S" -> moveSouth();
-                case "W" -> moveWest();
+                case "N" -> position = moveNorth();
+                case "E" -> position = moveEast(position);
+                case "S" -> position = moveSouth(position);
+                case "W" -> position = moveWest(position);
                 default -> throw new UnsupportedOperationException("Direction " + direction + " is not supported.");
             }
         }
         return position.getX() + ":" + position.getY() + ":" + direction;
     }
 
-    private void moveWest() {
+    private Position moveWest(Position position) {
         position.setX(position.getX() - 1);
+        return position;
     }
 
-    private void moveSouth() {
+    private Position moveSouth(Position position) {
         position.setY(position.getY() - 1);
+        return position;
     }
 
-    private void moveEast() {
+    private Position moveEast(Position position) {
         position.setX(position.getX() + 1);
+        return position;
     }
 
-    private void moveNorth() {
+    private Position moveNorth() {
         position.setY(position.getY() + 1);
+        return position;
     }
 }
