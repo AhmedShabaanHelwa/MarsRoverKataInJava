16e803ab636303309d5e4e239f8060746d1dcbe9
Ahmed Shaban Helwa <ahmedshabaan@outlook.com>
2022-06-17 19:26:54 +0200

    Feature 2: Cycle 3: Refactor: Switch-code smell: 7. Introduce the right amount of duplication so that extracting the methods from cases.

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -2,8 +2,9 @@ class MarsRover {
 
     private final String initialState;
     private Position position;
+    private IDirection direction;
 
-     MarsRover(String initialState) {
+    MarsRover(String initialState) {
         this.initialState = initialState;
         position = new Position();
     }
@@ -13,17 +14,29 @@ class MarsRover {
         String[] initialStateStrings = initialState.split(":");
         position.setX(Integer.parseInt(initialStateStrings[0]));
         position.setY(Integer.parseInt(initialStateStrings[1]));
-        String direction = initialStateStrings[2];
+        String directionCharacter = initialStateStrings[2];
 
         if (commands.equals("M")) {
-            switch (direction) {
-                case "N" -> position = new North().moveForward(position);
-                case "E" -> position = new East().moveForward(position);
-                case "S" -> position = new South().moveForward(position);
-                case "W" -> position = new West().moveForward(position);
+            switch (directionCharacter) {
+                case "N" -> {
+                    direction = new North();
+                    position = direction.moveForward(position);
+                }
+                case "E" -> {
+                    direction = new East();
+                    position = direction.moveForward(position);
+                }
+                case "S" -> {
+                    direction = new South();
+                    position = direction.moveForward(position);
+                }
+                case "W" -> {
+                    direction = new West();
+                    position = direction.moveForward(position);
+                }
                 default -> throw new UnsupportedOperationException("Direction " + direction + " is not supported.");
             }
         }
-        return position.getX() + ":" + position.getY() + ":" + direction;
+        return position.getX() + ":" + position.getY() + ":" + directionCharacter;
     }
 }
