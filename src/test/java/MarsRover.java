7a6d76b2e4839154b7e343183897e4b8c8a76d0a
Ahmed Shaban Helwa <ahmedshabaan@outlook.com>
2022-06-17 18:17:32 +0200

    Feature 2: Cycle 3: Refactor: Solve Primitive Obsession code smell by introducing Position class.

--- src/test/java/MarsRover.java
+++ src/test/java/MarsRover.java
@@ -1,35 +1,30 @@
 class MarsRover {
 
     private final String initialState;
+    private final Position position;
 
     public MarsRover(String initialState) {
 
         this.initialState = initialState;
+        position = new Position();
     }
 
     public String execute(String commands) {
 
         String[] initialStateStrings = initialState.split(":");
-        int x = Integer.parseInt(initialStateStrings[0]);
-        int y = Integer.parseInt(initialStateStrings[1]);
+        position.setX(Integer.parseInt(initialStateStrings[0]));
+        position.setY(Integer.parseInt(initialStateStrings[1]));
         String direction = initialStateStrings[2];
 
-        if(commands.equals("M")){
-            switch (direction){
-                case "N":
-                    y += 1;
-                    break;
-                case "E":
-                    x += 1;
-                    break;
-                case "S":
-                    y -= 1;
-                    break;
-                case "W":
-                    x -= 1;
-                    break;
+        if (commands.equals("M")) {
+            switch (direction) {
+                case "N" -> position.setY(position.getY() + 1);
+                case "E" -> position.setX(position.getX() + 1);
+                case "S" -> position.setY(position.getY() - 1);
+                case "W" -> position.setX(position.getX() - 1);
+                default -> throw new UnsupportedOperationException("Direction " + direction + " is not supported.");
             }
         }
-        return x + ":" + y + ":" + direction;
+        return position.getX() + ":" + position.getY() + ":" + direction;
     }
 }
---
+++ src/test/java/Position.java
@@ -0,0 +1,24 @@
+public class Position {
+    private int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    private int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Position() {
+    }
+}
