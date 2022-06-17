9d852299e06af265f5a959662bce6cc5ffc94b4b
Ahmed Shaban Helwa <ahmedshabaan@outlook.com>
2022-06-17 11:48:41 +0200

    Feature 2: Move Forward. Cycle 2: Green: Parse the initialState inside execute() and increment the corresponding direction.

--- src/test/java/MarsRover.java
+++ src/test/java/MarsRover.java
@@ -9,8 +9,13 @@ class MarsRover {
 
     public String execute(String commands) {
 
+        String[] initialStateStrings = initialState.split(":");
+        int x = Integer.parseInt(initialStateStrings[0]);
+        int y = Integer.parseInt(initialStateStrings[1]);
+        String direction = initialStateStrings[2];
+
         if(commands.equals("M")){
-            return "0:1:N";
+            return x + ":" + (++y) + ":" + direction;
         }
         return initialState;
     }
